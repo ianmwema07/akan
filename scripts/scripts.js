2a760b1c98cb73b56574a3e3bcbaeea5fda3f3f0
iano <ianmwema07@gmail.com>
2022-03-04 17:13:05 +0300

    add male names to array

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,17 @@
 function display (){
   var dateOfBirth = document.getElementById("dateOfBirth").value;
-  var dateArray = [];
-  dateArray.push(dateOfBirth);
   var sexMale = document.getElementById("male").checked;
   var sexFemale = document.getElementById("female").checked;
+ /**cc */ var centuryOfBirth = parseInt(dateOfBirth.slice(0,2)) ;
+ /**yy */ var yearOfBirth = parseInt(dateOfBirth.slice(2,4));
+ /**mm */ var monthOfBirth = parseInt(dateOfBirth.slice(5,7));
+ /**dd */ var dayOfBirth =  parseInt(dateOfBirth.slice(8,11));
+
+ var dayOfTheWeek = (((centuryOfBirth/4) -2*centuryOfBirth-1) + ((5*yearOfBirth/4)) + ((26*(monthOfBirth+1)/10)) + dayOfBirth ) % 7;
+ var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 
   if(sexMale == true){
-    alert("Male");
+    alert("Male"+dayOfBirth +"  "+ dateOfBirth +" "+ dayOfTheWeek);
   }else if(sexFemale == true) {
     alert("Female");
   }
