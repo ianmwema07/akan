f5147b0029342269c5ac3941661fcc03158b5613
iano <ianmwema07@gmail.com>
2022-03-04 17:42:27 +0300

    finished adding the logic for checking the day of the week

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,25 +11,45 @@ function display (){
  var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
  var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
   if(sexMale == true){
-    if(dayOfTheWeek == 1) {
-      alert();
-    } else if(dayOfTheWeek == 2){
 
+    
+    if(dayOfTheWeek == 1) {
+      // alert(maleAkanNames[0] + dayOfTheWeek);
+      alert("You are male and you were born on Sunday therefore your Akan name is "+maleAkanNames[0]);
     } else if(dayOfTheWeek == 2){
-      
-    } else if(dayOfTheWeek == 2){
-      
-    } else if(dayOfTheWeek == 2){
-      
-    } else if(dayOfTheWeek == 2){
-      
-    } else if(dayOfTheWeek == 2){
-      
-    }else {
+      alert("You are male and you were born on Monday therefore your Akan name is "+maleAkanNames[1]);
+    }
+     else if(dayOfTheWeek == 3){
+      alert("You are male and you were born on Tuesday therefore your Akan name is "+maleAkanNames[2]);
+    } else if(dayOfTheWeek == 4){
+      alert("You are male and you were born on Wednesday therefore your Akan name is "+maleAkanNames[3]);
+    } else if(dayOfTheWeek == 5){
+      alert("You are male and you were born on Thursday therefore your Akan name is "+maleAkanNames[4]);
+    } else if(dayOfTheWeek == 6){
+      alert("You are male and you were born on Friday therefore your Akan name is "+maleAkanNames[5]);
+    } else if(dayOfTheWeek == 7){
+      alert("You are male and you were born on Saturday therefore your Akan name is "+maleAkanNames[6]);
+    } else {
       alert("Invalid Entry");
     }
-  }else if(sexFemale == true) {
-    alert("Female");
+  } else if(sexFemale == true) {
+    if(dayOfTheWeek == 1) {
+      alert("You are female and you were born on Sunday therefore your Akan name is "+femaleAkanNames[0]);
+    } else if(dayOfTheWeek == 2){
+      alert("You are female and you were born on Monday therefore your Akan name is "+femaleAkanNames[1]);
+    } else if(dayOfTheWeek == 3){
+      alert("You are female and you were born on Tuesday therefore your Akan name is "+femaleAkanNames[2]);
+    } else if(dayOfTheWeek == 4){
+      alert("You are female and you were born on Wednesday therefore your Akan name is "+femaleAkanNames[3]);
+    } else if(dayOfTheWeek == 5){
+      alert("You are female and you were born on Thursday therefore your Akan name is "+femaleAkanNames[4]);
+    } else if(dayOfTheWeek == 6){
+      alert("You are female and you were born on Friday therefore your Akan name is "+femaleAkanNames[5]);
+    } else if(dayOfTheWeek == 7){
+      alert("You are female and you were born on Saturday therefore your Akan name is "+femaleAkanNames[6]);
+    } else {
+      alert("Invalid Entry");
+    }
   }
   
 }
