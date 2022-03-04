f7345c77bc2d75ba3dae3d1ffbcd17babb43070b
iano <ianmwema07@gmail.com>
2022-03-04 17:21:46 +0300

    added if else statement to check whether the value added is for a male or female

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,11 +7,27 @@ function display (){
  /**mm */ var monthOfBirth = parseInt(dateOfBirth.slice(5,7));
  /**dd */ var dayOfBirth =  parseInt(dateOfBirth.slice(8,11));
 
- var dayOfTheWeek = (((centuryOfBirth/4) -2*centuryOfBirth-1) + ((5*yearOfBirth/4)) + ((26*(monthOfBirth+1)/10)) + dayOfBirth ) % 7;
+ var  dayOfTheWeek =  parseInt(Math.ceil((((centuryOfBirth/4) -2*centuryOfBirth-1) + ((5*yearOfBirth/4)) + ((26*(monthOfBirth+1)/10)) + dayOfBirth ) % 7)); 
  var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-
+ var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
   if(sexMale == true){
-    alert("Male"+dayOfBirth +"  "+ dateOfBirth +" "+ dayOfTheWeek);
+    if(dayOfTheWeek == 1) {
+      alert();
+    } else if(dayOfTheWeek == 2){
+
+    } else if(dayOfTheWeek == 2){
+      
+    } else if(dayOfTheWeek == 2){
+      
+    } else if(dayOfTheWeek == 2){
+      
+    } else if(dayOfTheWeek == 2){
+      
+    } else if(dayOfTheWeek == 2){
+      
+    }else {
+      alert("Invalid Entry");
+    }
   }else if(sexFemale == true) {
     alert("Female");
   }
